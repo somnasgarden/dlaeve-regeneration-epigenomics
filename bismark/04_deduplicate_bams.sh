3088d38ebbf64250d01d1e030a6151a57271a774
jerolon <37126052+jerolon@users.noreply.github.com>
2026-04-07 16:08:55 -0600

    Create 04_deduplicate_bams.sh

---
+++ bismark/04_deduplicate_bams.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+#SBATCH --job-name=dedup      # Job name
+#SBATCH --output=slurm_logs/dedup_%A_%a.out  # Standard output log
+#SBATCH --error=slurm_logs/dedup_%A_%a.err   # Standard error log
+#SBATCH --array=1-4                   # Run 4 tasks, numbered 1, 2, 3, 4
+#SBATCH --ntasks=1                    # Each task is a single task
+#SBATCH --cpus-per-task=2           # Request 8 CPUs for this task
+#SBATCH --mem=100G                  # Request 100GB of memory
+
+# --- SAFETY FIRST ---
+# Stop script on any error
+set -e
+# Treat unset variables as errors
+set -u
+
+echo "Starting Deduplication of BAMs to remove excessive PCR amplification..."
+echo "Starting job $SLURM_JOB_ID, array task $SLURM_ARRAY_TASK_ID"
+echo "Running on host: $(hostname)"
+echo "Allocated CPUs: $SLURM_CPUS_PER_TASK"
+echo "Allocated Memory: $SLURM_MEM_PER_NODE"
+
+module load bowtie2/2.5.4 samtools/1.22.1
+export PATH=/mnt/data/.../80-scripts/81-bin/Bismark-0.25.1:$PATH
+export PERL5LIB=$HOME/.perl/lib:$PERL5LIB
+
+SAMPLE_IDS=("A1" "A2" "C1" "C2")
+
+CURRENT_INDEX=$((SLURM_ARRAY_TASK_ID - 1))
+CURRENT_ID=${SAMPLE_IDS[$CURRENT_INDEX]}
+
+echo "Processing Sample ID: $CURRENT_ID"
+
+INPUT_DIR="06_bismark_alignment"
+MAIN_OUTPUT_DIR="08_deduplication" # Main output folder
+
+INPUT_BAM="${INPUT_DIR}/trimmed_${CURRENT_ID}_R1.paired_bismark_bt2_pe.bam"
+#SAMPLE_OUTPUT_DIR="${MAIN_OUTPUT_DIR}/${CURRENT_ID}_output" # Create a unique output dir for this sample
+
+# --- CREATE DIRECTORIES ---
+# Create the main output and slurm log directories
+mkdir -p $MAIN_OUTPUT_DIR
+#mkdir -p $SAMPLE_OUTPUT_DIR
+#mkdir -p slurm_logs
+
+# --- RUN THE COMMAND ---
+echo "Input file: $INPUT_BAM"
+echo "Output directory: $MAIN_OUTPUT_DIR"
+
+deduplicate_bismark --outfile "${CURRENT_ID}_paired_bismark_bt2_pe" --output_dir $MAIN_OUTPUT_DIR $INPUT_BAM
+
+echo "Finished processing $CURRENT_ID."
